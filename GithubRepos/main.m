1cf3e235ce069eb6892aa91bfec07d97d17804f6
olga-nest <olga.w.nesterova@gmail.com>
2017-10-23 18:38:29 -0700

    Initial Commit

---
+++ GithubRepos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GithubRepos
+//
+//  Created by Olga on 10/23/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
