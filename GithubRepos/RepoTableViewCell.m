4faf904bc923876fd5f7196a57d67df11fc59221
olga-nest <olga.w.nesterova@gmail.com>
2017-10-23 19:06:52 -0700

    Add RepoTableViewCell class

---
+++ GithubRepos/RepoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RepoTableViewCell.m
+//  GithubRepos
+//
+//  Created by Olga on 10/23/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "RepoTableViewCell.h"
+
+@implementation RepoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
